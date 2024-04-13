9a16bda4039a8bf3f6a504272699fb84085d5aa6
ivansmorales <ivann_mg@hotmail.com>
2024-04-12 20:03:25 -0600

    Add CongratsModal logic to show credits

--- src/wizard/CongratsModal.tsx
+++ src/wizard/CongratsModal.tsx
@@ -1,19 +1,57 @@
 import React from "react";
-import { Modal, StyleSheet, View } from "react-native";
+import { FlatList, Modal, StyleSheet, View } from "react-native";
 
 import CustomButton from "../components/CustomButton";
 import CustomText from "../components/CustomText";
+import theme from "../../theme";
+import useCredits from "../hooks/useCredits";
+import { Credit } from "../types";
 
-export default function CongratsModal({ onSuccess }: any) {
+type Props = {
+  onSuccess: () => void;
+  selectedCredit: { id: number; amount: number } | null;
+  setSelectedCredit: React.Dispatch<React.SetStateAction<Credit | null>>;
+};
+
+export default function CongratsModal({
+  onSuccess,
+  selectedCredit,
+  setSelectedCredit,
+}: Props) {
+  const { loading, credits } = useCredits();
+
   return (
-    <View style={styles.centeredView}>
+    <View style={styles.wrapper}>
       <Modal animationType="slide" transparent={true} visible={true}>
-        <View style={styles.centeredView}>
+        <View style={styles.wrapper}>
           <View style={styles.modalView}>
-            <CustomText fontSize="header">¡Felicidades!</CustomText>
-            <CustomText>
+            <CustomText fontSize="large">¡Felicidades!</CustomText>
+            <CustomText variant="secondary">
               Encontramos estos créditos perfectos para ti:
             </CustomText>
+            {loading && <CustomText>Cargando...</CustomText>}
+            <FlatList
+              data={credits}
+              renderItem={({ item, index }: any) => {
+                // Fixing issue from API on duplicated id's
+                const idx = index + 1;
+                return (
+                  <CustomButton
+                    key={index}
+                    title={`Crédito ${idx}  $${item.amount}`}
+                    onPress={() =>
+                      setSelectedCredit({ id: idx, amount: item.amount })
+                    }
+                    style={{
+                      backgroundColor:
+                        selectedCredit?.id === idx
+                          ? theme.colors.btnPrimary
+                          : theme.colors.grey,
+                    }}
+                  />
+                );
+              }}
+            />
             <CustomButton
               title="Seleccionar crédito"
               onPress={() => onSuccess()}
@@ -26,18 +64,14 @@ export default function CongratsModal({ onSuccess }: any) {
 }
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
+  wrapper: {
+    marginTop: 100,
   },
   modalView: {
     margin: 20,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
+    padding: 15,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
